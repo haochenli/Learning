753e88e2e8d03d5d448b3d009eb64a789757dac0
Haochen Li <lihaochen2017@outlook.com>
2020-01-31 18:40:38 +0800

    Slave (#1)
    * add dynamic programming
    
    * add curified

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -14,3 +14,15 @@ function getFibonacci(n) {
 }
 
 console.log(getFibonacci(4))
+
+// 求最小子数组和的问题  例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
+function FindGreatestSumOfSubArray(array){
+  let pre = array[0]
+  let max = array[0]
+  for(let i = 1 ; i < array.length ; i++) {
+    pre = pre > 0 ? pre : 0
+    max = Math.max(max, pre + array[i])
+    pre = pre + array[i]
+  }
+  return max
+}
