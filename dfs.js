5a8a4978d7a7469dfea88114691a2b30d4329a3a
HaochenLi <lihaochen2017@outlook.com>
2020-02-01 21:42:16 +0800

    add dfs and bfs

---
+++ dfs.js
@@ -0,0 +1,10 @@
+function deepTraversal(node,nodeList) {  
+  if (node) {    
+          nodeList.push(node);    
+          var children = node.children;    
+          for (var i = 0; i < children.length; i++) 
+    //每次递归的时候将  需要遍历的节点  和 节点所存储的数组传下去
+              deepTraversal(children[i],nodeList);    
+      }    
+  return nodeList;  
+}  
