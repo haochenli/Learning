cdaea9dc6b0d60dbd5c58293d9c6d4cc49a39fc7
HaochenLi <lihaochen2017@outlook.com>
2020-02-23 15:18:24 +0800

    add sth to bfs and dfs

--- dfs.js
+++ dfs.js
@@ -8,3 +8,33 @@ function deepTraversal(node,nodeList) {
       }    
   return nodeList;  
 }  
+
+function deepTraversal(node) {  
+  var nodeList = [];  
+  if (node) {  
+      var stack = [];  
+      stack.push(node);  
+      while (stack.length != 0) {  
+          var childrenItem = stack.pop();  
+          nodeList.push(childrenItem);  
+          var childrenList = childrenItem.children;  
+          for (var i = childrenList.length - 1; i >= 0; i--)  
+              stack.push(childrenList[i]);  
+      }  
+  }    
+  return nodeList;  
+}   
+
+function inorderTraverse (root) {
+  let result = []
+  let inorder = (node) => {
+    if(node) {
+      inorder(node.left)
+      // inorder push should be the second, preorder should be the first,post shold be the last
+      result.push(node.val)
+      inorder(node.right)
+    }
+  }
+  inorder(root)
+  return result
+}
